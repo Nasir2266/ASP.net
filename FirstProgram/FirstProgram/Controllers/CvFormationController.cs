8ecf39c1efd8c6966c0c60675421abdde46997f4
unknown <abdullahnasif07@gmail.com>
2022-09-23 23:10:25 +0600

    Task1

---
+++ FirstProgram/FirstProgram/Controllers/CvFormationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstProgram.Controllers
+{
+    public class CvFormationController : Controller
+    {
+        // GET: CvFormation
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        public ActionResult AboutMe()
+        {
+            return View();
+        }
+
+        public ActionResult Experience()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+    }
+}
